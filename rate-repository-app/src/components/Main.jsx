2670003372e93b8598207b2d8ace8f5918344ba7
kannvess <kannvess@gmail.com>
2023-08-14 17:54:32 +0800

    Finished exercise Exercise 10.5: polished reviewed repositories list

--- rate-repository-app/src/components/Main.jsx
+++ rate-repository-app/src/components/Main.jsx
@@ -1,12 +1,12 @@
 import { StyleSheet, View } from 'react-native';
 import RepositoryList from './RepositoryList';
-import Text from './Text';
 import AppBar from './AppBar';
 
 const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
     flexShrink: 1,
+    backgroundColor: '#e1e4e8',
   },
 });
 
@@ -14,7 +14,6 @@ const Main = () => {
   return (
     <View style={styles.container}>
       <AppBar />
-      <Text>Rate Repository Application</Text>
       <RepositoryList />
     </View>
   );
--- rate-repository-app/src/components/RepositoryItem.jsx
+++ rate-repository-app/src/components/RepositoryItem.jsx
@@ -1,15 +1,36 @@
-import { Text, View } from "react-native";
+import { Image, View } from "react-native";
+import Text from "./Text";
+import theme from "../theme";
 
 const RepositoryItem = ({ item }) => {
   return (
-    <View>
-      <Text>Full name: {item.fullName}</Text>
-      <Text>Description: {item.description}</Text>
-      <Text>Language: {item.language}</Text>
-      <Text>Stars: {item.stargazersCount}</Text>
-      <Text>Forks: {item.forksCount}</Text>
-      <Text>Reviews: {item.reviewCount}</Text>
-      <Text>Rating: {item.ratingAverage}</Text>
+    <View style={{ backgroundColor: 'white', padding: 10 }}>
+      <View style={{ display: "flex", flexDirection: 'row' }}>
+        <Image source={{uri: item.ownerAvatarUrl}} style={{ width: 50, height: 50, borderRadius: 5 }} />
+        <View style={{ marginLeft: 10, flexShrink: 1, alignItems: "flex-start" }}>
+          <Text style={{ marginBottom: 5 }} fontWeight={'bold'}>{item.fullName}</Text>
+          <Text style={{ marginBottom: 5 }}>{item.description}</Text>
+          <Text style={{ backgroundColor: theme.colors.primary, color: 'white', paddingVertical: 2, paddingHorizontal: 4, borderRadius: 5 }}>{item.language}</Text>
+        </View>
+      </View>
+      <View style={{ paddingTop: 10, flexDirection: 'row', justifyContent: "space-around" }}>
+        <View style={{ alignItems: 'center' }}>
+          <Text fontWeight={'bold'}>{item.stargazersCount >= 1000 ? `${(item.stargazersCount / 1000).toFixed(1)}k` : item.stargazersCount}</Text>
+          <Text>Stars</Text>
+        </View>
+        <View style={{ alignItems: 'center' }}>
+          <Text fontWeight={'bold'}>{item.forksCount >= 1000 ? `${(item.forksCount / 1000).toFixed(1)}k` : item.forksCount}</Text>
+          <Text>Forks</Text>
+        </View>
+        <View style={{ alignItems: 'center' }}>
+          <Text fontWeight={'bold'}>{item.reviewCount}</Text>
+          <Text>Reviews</Text>
+        </View>
+        <View style={{ alignItems: 'center' }}>
+          <Text fontWeight={'bold'}>{item.ratingAverage}</Text>
+          <Text>Rating</Text>
+        </View>
+      </View>
     </View>
   );
 };
