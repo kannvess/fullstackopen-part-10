9b7d6cb9f1e90b4825677b6179b3089e8f72244f
kannvess <kannvess@gmail.com>
2023-08-19 22:22:58 +0800

    Finished Exercise 10.8: the sign-in form

---
+++ rate-repository-app/src/components/BodyMassIndexCalculator.jsx
@@ -0,0 +1,44 @@
+import { Formik } from "formik";
+import { Pressable, View } from "react-native";
+import Text from "./Text";
+import FormikTextInput from "./FormikTextInput";
+
+const initialValues = {
+  mass: '',
+  height: '',
+};
+
+const getBodyMassIndex = (mass, height) => {
+  return Math.round(mass / Math.pow(height, 2));
+};
+
+const BodyMassIndexForm = ({ onSubmit }) => {
+  return (
+    <View>
+      <FormikTextInput name="mass" placeholder="Weight (kg)" />
+      <FormikTextInput name="height" placeholder="Height (m)" />
+      <Pressable onPress={onSubmit}>
+        <Text>Calculate</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+const BodyMassIndexCalculator = () => {
+  const onSubmit = (values) => {
+    const mass = parseFloat(values.mass);
+    const height = parseFloat(values.height);
+
+    if (!(isNaN(mass) || isNaN(height)) && height !== 0) {
+      console.log(`Your body mass index is: ${getBodyMassIndex(mass, height)}`);
+    }
+  };
+
+  return (
+    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      {({ handleSubmit }) => <BodyMassIndexForm onSubmit={handleSubmit} />}
+    </Formik>
+  );
+};
+
+export default BodyMassIndexCalculator;
--- rate-repository-app/src/components/SignIn.jsx
+++ rate-repository-app/src/components/SignIn.jsx
@@ -1,7 +1,59 @@
+import { Pressable, StyleSheet, View } from "react-native";
 import Text from "./Text";
+import FormikTextInput from "./FormikTextInput";
+import { Formik } from "formik";
+import theme from "../theme";
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    backgroundColor: 'white',
+  },
+  textInput: {
+    marginVertical: 10,
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: 'gray',
+  },
+  signInButton: {
+    backgroundColor: theme.colors.primary,
+    color: 'white',
+    borderRadius: 5,
+    paddingVertical: 10,
+    textAlign: 'center',
+  },
+});
+
+const initialValues = {
+  username: '',
+  password: '',
+};
+
+const SignInForm = ({ onSubmit }) => {
+  return (
+    <View style={styles.container}>
+      <FormikTextInput style={styles.textInput} name="username" placeholder="Username" />
+      <FormikTextInput style={styles.textInput} name="password" placeholder="Password" secureTextEntry={true} />
+      <Pressable onPress={onSubmit}>
+        <Text fontWeight={'bold'} style={styles.signInButton}>Sign in</Text>
+      </Pressable>
+    </View>
+  );
+};
 
 const SignIn = () => {
-  return <Text>The sign-in view</Text>;
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+  
+  return (
+    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
+    </Formik>
+  );
 };
 
 export default SignIn;
